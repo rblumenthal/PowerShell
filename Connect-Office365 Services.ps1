03d530a076527b288c86c3552e1504277b81bc43
rblumenthal <russell.blumenthal@gmail.com>
2017-03-21 12:10:59 -0400

    Add scripts
    Add vCenter and O365 connect scripts

---
+++ Connect-Office365 Services.ps1
@@ -0,0 +1,14 @@
+﻿# Exchange Online
+Import-PSSession $(New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $(Import-Clixml -Path "$home\Documents\office365.xml") -Authentication Basic -AllowRedirection)
+
+#Compliance and Security Center
+Import-PSSession $(New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid -Credential $(Import-Clixml -Path "$home\Documents\office365.xml") -Authentication Basic -AllowRedirection) -AllowClobber -DisableNameChecking 
+
+# SharePoint
+Connect-SPOService -Url https://stonybrookmedicine-admin.sharepoint.com -Credential $(Import-Clixml -Path "$home\Documents\office365.xml") -Authentication Basic -AllowRedirection
+
+# Skype
+Import-PSSession $(New-CsOnlineSession -Credential $(Import-Clixml -Path "$home\Documents\office365.xml"))
+
+# Microsoft Online
+ Connect-MsolService -Credential $(Import-Clixml -Path "$home\Documents\office365.xml") 
---
+++ vCenter Connect.ps1
@@ -0,0 +1,28 @@
+﻿if (Get-PSSnapin vmware* -ErrorAction SilentlyContinue) {
+Write-Host "vSphere snapin already loaded, proceeding."
+} else {
+Write-Host "Loading vSphere snapin."
+Add-pssnapin VMWare.VimAutomation.Core -ErrorAction SilentlyContinue
+    if (Get-PSSnapin vmware* -ErrorAction SilentlyContinue) {
+    Write-Host "vSphere snapin loaded"
+    } else {
+    Write-Host -ForegroundColor Red "Error loading vSphere snapin. Halting."
+    Write-Host -ForegroundColor Red "VMware PowerCLI is required to run this script."
+    break
+    }
+ }
+
+    $vcenter = Read-Host "Please enter the IP for your vCenter server or VMware host"
+    $date = Get-Date -uformat "%m-%d-%Y"
+
+    if (Test-Connection -ComputerName $vcenter -Count 1){
+    Write-Host "Please wait while we attempt to connect to $vcenter"
+    Connect-VIServer -Server $vcenter <#-User $vcusername -Password $pw#> > $NULL 2>&1
+    if (!($vcusername)) { $vcusername = [Environment]::UserName }
+    Write-Host "Connected"
+    } else {
+    Write-Host -ForegroundColor Red "Halt: $vcenter is not reachable."
+    break
+    }
+
+    
